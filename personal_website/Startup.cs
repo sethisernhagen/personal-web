8190a2307f1bd8492766d6122bdbc6e09e6189e7
Isernhagen, Seth <sisernha@Advent.COM>
2015-02-20 14:35:02 -0800

    first push

---
+++ personal_website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(personal_website.Startup))]
+namespace personal_website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
