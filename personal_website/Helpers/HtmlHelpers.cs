4ecd3f0c5057679cc1ec251923aa0d8b9495d32b
Isernhagen, Seth <sisernhagen@gmail.com>
2015-03-08 10:51:03 -0700

    added htmlhelper; new favicon

---
+++ personal_website/Helpers/HtmlHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace personal_website.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static string MakeActiveClassAction(this UrlHelper urlHelper, string action)
+        {
+            string result = "active";
+
+            string actionName = urlHelper.RequestContext.RouteData.Values["action"].ToString();
+
+            if (!actionName.Equals(action, StringComparison.OrdinalIgnoreCase))
+            {
+                result = null;
+            }
+
+            return result;
+        }
+
+    }
+}
